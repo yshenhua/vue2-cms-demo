13ceb959bcafdf7f8dace44ee2456d8c0667687e
文燚 <yshenhua@gmail.com>
2017-12-03 22:20:19 +0800

    全屏布局

---
+++ src/components/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    <header class="top"></header>
+    <nav class="left"></nav>
+    <article class="right"></article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/components/Layout'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '首页',
+      component: Layout,
+      children: []
     }
   ]
 })
