cb37011de39bb0f9281554cf213c9add3eaf8f19
文燚 <yshenhua@gmail.com>
2017-12-17 16:05:50 +0800

    把main.js中的配置放到configs文件夹中

---
+++ src/configs/element-ui.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+
+import {
+  Form,
+  FormItem,
+  Input,
+  Radio,
+  RadioGroup,
+  Checkbox,
+  CheckboxGroup,
+  Select,
+  Option,
+  Button,
+  DatePicker,
+  TimePicker,
+  Popover,
+  Tooltip,
+  MessageBox
+} from 'element-ui'
+
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Button)
+Vue.use(DatePicker)
+Vue.use(TimePicker)
+Vue.use(Popover)
+Vue.use(Tooltip)
+Vue.prototype.$msgbox = MessageBox
